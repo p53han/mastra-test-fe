d4d2b5508668c11a9bf877232f4dead9d0c3e7c9
Han <han@DLs-MacBook-Pro.local>
2025-07-16 18:49:34 -0400

    thread suggestion text updated

--- components/assistant-ui/thread.tsx
+++ components/assistant-ui/thread.tsx
@@ -87,12 +87,12 @@ const ThreadWelcomeSuggestions: FC = () => {
     <div className="mt-3 flex w-full items-stretch justify-center gap-4">
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="What is the weather in Tokyo?"
+        prompt="What is Mastra AI?"
         method="replace"
         autoSend
       >
         <span className="line-clamp-2 text-ellipsis text-sm font-semibold">
-          What is the weather in Tokyo?
+          What is Mastra AI?
         </span>
       </ThreadPrimitive.Suggestion>
       <ThreadPrimitive.Suggestion
